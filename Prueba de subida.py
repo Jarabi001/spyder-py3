34fc3b80c2cef2fca09d51f7fc0005cf894bc7aa
Jarabi001 <jarabi02897@gmail.com>
2025-01-31 13:32:24 -0500

    Se agrega el proyecto

---
+++ Prueba de subida.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 13:23:02 2025
+
+@author: Usuario
+"""
+
+print("hola mundo")
+x=1
+y=2
+suma= x+y
+print("la suma es:", suma)
+
+nombre= input ("ingresa tu nombre y apellido:")
+print("Hola", nombre)
